7cdd80ed34e33097d4f221dd8023b0ee27c15522
AnnupKapur <AnnupKapur@gmail.com>
2024-12-02 12:03:57 +0000

    rust - day1 - solutionB

--- 2024/Rust/src/day_1/mod.rs
+++ 2024/Rust/src/day_1/mod.rs
@@ -1,5 +1,6 @@
 use std::io::{BufRead, BufReader};
 use std::fs::File;
+use std::collections::HashMap;
 
 pub fn solution_a () {
     let mut left_list = Vec::new();
@@ -33,3 +34,45 @@ pub fn solution_a () {
     println!("{}", sum);
 
 }
+
+pub fn solution_b () {
+    let mut left_list = Vec::new();
+    let mut right_count_map = HashMap::new();
+
+    let reader = BufReader::new(File::open("data/day1.txt").expect("Cannot open file.txt"));
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.trim().is_empty() {
+            continue;
+        }
+
+        if let Some((left, right)) = line.split_once("   ") {
+            left_list.push(left.trim().parse::<i32>().unwrap());
+            if right_count_map.contains_key(&right.trim().parse::<i32>().unwrap()){
+                right_count_map
+                    .insert(
+                        right.trim().parse::<i32>().unwrap(), 
+                        right_count_map.get(&right.trim().parse::<i32>().unwrap()).unwrap() + 1
+                    );
+            } else {
+                right_count_map.insert(right.trim().parse::<i32>().unwrap(), 1);
+            }
+        }
+    }
+
+    let mut multiples: Vec<i32> = Vec::new();
+
+    left_list.iter().enumerate().for_each(|(_i, l)| {
+        if right_count_map.contains_key(l) {
+            multiples.push(right_count_map.get(l).unwrap() * l);
+        }
+    });
+
+    let sum: i32 = multiples.iter().sum();
+
+    println!("{}", sum);
+}
+
+
+
