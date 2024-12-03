9a2bfc78ab48b1fdcb9cd1564f9067f12d40bb32
AnnupKapur <AnnupKapur@gmail.com>
2024-12-03 00:05:04 +0000

    js - day2 - solutionA

---
+++ 2024/JS/Day2/solutionA.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+const isValidIncrease = (line) => {
+  if (line.length === 1) return true;
+  if (line[0] - line[1] >= -3 && line[0] - line[1] < 0)
+    return isValidIncrease(line.slice(1));
+  return false;
+};
+
+const isValidDecrease = (line) => {
+  if (line.length === 1) return true;
+  if (line[0] - line[1] <= 3 && line[0] - line[1] > 0)
+    return isValidDecrease(line.slice(1));
+  return false;
+};
+
+fs.readFile("../../data/day2.txt", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const lines = data.toString().split("\n").slice(0, -1);
+  const res = lines.reduce((acc, l) => {
+    const line = l.split(" ");
+    if (isValidIncrease(line) || isValidDecrease(line)) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+
+  console.log(`Result: ${res}`);
+});
