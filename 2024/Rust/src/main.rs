c077c7a91a21cfb0ed151d45b637c62865bc7fd9
AnnupKapur <AnnupKapur@gmail.com>
2024-12-03 22:54:35 +0000

    rust - day2 - solutionA & solutionB

--- 2024/Rust/src/main.rs
+++ 2024/Rust/src/main.rs
@@ -1,8 +1,15 @@
 mod day_1;
+mod day_2;
 
 fn main() {
+    println!("============================");
     println!("Advent of Code 2024!");
+    println!("============================");
     println!("Day 1:");
     day_1::solution_a();
     day_1::solution_b();
+    println!("----------------------------");
+    println!("Day 2:");
+    day_2::solution_a();
+    day_2::solution_b();
 }
