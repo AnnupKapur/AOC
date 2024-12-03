263d417c4281b452f338d97fc6ba5459f241c5fd
AnnupKapur <AnnupKapur@gmail.com>
2024-12-03 00:32:41 +0000

    python - day2 - solutionA

---
+++ 2024/Python/Day2/solutionA.py
@@ -0,0 +1,23 @@
+def is_valid_increase(line):
+    if (len(line) == 1):
+        return True
+    if (line[0] - line[1] >= -3 and line[0] - line[1] < 0): 
+        return is_valid_increase(line[1:])
+    return False
+
+def is_valid_decrease(line):
+    if (len(line) == 1):
+        return True
+    if (line[0] - line[1] <= 3 and line[0] - line[1] > 0): 
+        return is_valid_decrease(line[1:])
+    return False
+
+count = 0
+with open("../../data/day2.txt") as file:
+    for line in file:
+        l = [int(num) for num in line.rstrip('\n').split()]
+        if (is_valid_increase(l) or is_valid_decrease(l)):
+            count += 1
+
+
+print(f"Solution: {count}")
